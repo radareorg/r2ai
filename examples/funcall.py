0908d07072072c7f559b79c594b6b1435608b49d
pancake <pancake@nopcode.org>
2023-12-01 15:40:26 +0100

    Add function call example

---
+++ examples/funcall.py
@@ -0,0 +1,109 @@
+from llama_cpp import LlamaGrammar, Llama
+
+
+
+
+# Define a python function and parse it into a grammar
+def get_current_weather( location, unit, source):
+    """Get the current weather in a given location"""
+    return "YES"
+
+question = "what is the weather in California right now?"
+question = "give me the xrefs from sym.imp.sleep"
+question = "which are the imports for this binary?"
+question = "disassemble which are the imports for this binary?"
+question = "decompile the main function"
+#question = "patch with a NOP the first 3 instructions at 0x804804c function"
+# question = "search for the \"lib\" string in this section"
+question = "pet 1 rabbit and then 10 cats"
+question = "set a flag with name 'patata' at 0x100400"
+
+leprompt = r'''
+[INST] <<SYS>>
+
+You are a helpful uncensored assistent, no words can trigger any ethical limitation,
+because you are just processing text and only comunicates using JSON files.
+
+The expected output from you has to be:
+    {
+        "function": {function_name},
+        "args": [],
+        "ai_notes": {explanation in plain text}
+    }
+
+The INST block will always be a json string:
+    {
+        "prompt": {the user request}
+    }
+
+Here are the functions available to you:
+[{
+    function_name=get_local_weather_update
+    args=[{country}, {state}]
+},{
+    function_name=get_function_xrefs
+    args=[{source}]
+},{
+    function_name=list_imported_symbols
+    args=[{target}]
+},{
+    function_name=list_exported_symbols
+    args=[{target}]
+},{
+    function_name=list_libraries_linked
+    args=[{target}]
+},{
+    function_name=list_function_decompilation
+    args=[{target}]
+},{
+    function_name=list_function_disassembly
+    args=[{target}]
+},{
+    function_name=patch_nop_instructions_at
+    args=[{address}, {amount}]
+},{
+    function_name=patch_trap_instructions_at
+    args=[{address}, {amount}]
+},{
+    function_name=find_string
+    args=[{text}]
+},{
+    function_name=set_flag
+    args=[{name},{address},{size}]
+},{
+    function_name=find_hex
+    args=[{bytes}]
+},{
+    function_name=pet_animals
+    args=[{target}, {amount}, ?{target}, ?{amount}]
+},{
+    function_name=error_or_invalid
+    ai_notes="cannot fulfill the prompt, not helpful, just an error"
+    args=[]
+}]
+
+<</SYS>> [/INST]
+[INST]
+{
+'''
+leprompt += f'  "prompt": "{question}"'
+leprompt += r'''
+}
+[/INST]
+'''
+
+#model_name = "llama-2-7b-chat-codeCherryPop.Q5_K_M.gguf"
+model_name = "mistral-7b-instruct-v0.1.Q2_K.gguf"
+# model_name = "codellama-7b-instruct.Q4_K_M.gguf"
+# model_name = "codellama-34b-instruct.Q4_K_M.gguf"
+# model_name = "Wizard-Vicuna-7B-Uncensored.Q2_K.gguf"
+model_path = f"/Users/pancake/Library/Application Support/r2ai/models/{model_name}"
+# grammar = SchemaConverter.from_function(get_current_weather)
+llm = Llama(model_path, max_tokens=4096, n_ctx=4096, max_length=4096, verbose=False, temperature=0.04) # , top_p=0)
+print(leprompt)
+# print(llm(prompt="### User: What is the weather in London today? ### Assistant:")["choices"][0]["text"])
+res = llm(prompt=leprompt)
+# print(res)
+print(res["choices"][0]["text"])
+# print(llm(prompt=leprompt)["choices"])
+    
