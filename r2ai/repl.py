d593d1c3e330b3e7e01bcab16a976a021367d5ea
daniel nakov <daniel.nakov@gmail.com>
2024-02-28 16:16:42 -0500

    change :auto to quote space

--- r2ai/repl.py
+++ r2ai/repl.py
@@ -79,7 +79,7 @@ def start_http_server():
 help_message = """Usage: r2ai [-option] ([query] | [script.py])
  r2ai . [file]          interpret r2ai script with access to globals
  r2ai :aa               run a r2 command
- r2ai :auto [prompt]    query LLM that can interact with r2 (WIP, OpenAI only atm)
+ r2ai ' [prompt]        auto mode; query LLM that can interact with r2
  r2ai !ls               run a system command
  r2ai -a                query with audio voice
  r2ai -A                enter the voice chat loop
@@ -293,11 +293,11 @@ def runline(ai, usertext):
     except:
       traceback.print_exc()
       pass
-  elif usertext.startswith(":auto"):
+  elif usertext.startswith("' "):
     if not autoai:
       autoai = r2ai.interpreter.Interpreter()
       autoai.auto_run = True
-      autoai.chat(usertext[5:])
+      autoai.chat(usertext[2:])
   elif usertext[0] == ":":
     if r2 is None:
       print("r2 is not available")
