d7842574ad2e6dff929d90f74c6c1a0adc18c468
pancake <pancake@nopcode.org>
2024-04-20 02:25:20 +0200

    Fixes for plugin vs shell uses

--- main.py
+++ main.py
@@ -2,15 +2,13 @@ import runpy
 import sys
 import os
 
-r2aihome = os.path.dirname(__file__) #os.readlink(__file__))
-if "PYTHONPATH" in os.environ:
-  os.environ["PYTHONPATH"] = r2aihome + ":" + os.environ["PYTHONPATH"]
-else:
-  os.environ["PYTHONPATH"] = r2aihome
-if "VIRTUAL_ENV" in os.environ:
+r2aihome = os.path.dirname(__file__)
+try:
+  r2aihome = os.path.dirname(os.readlink(__file__))
+except:
+  pass
+sys.path.insert(0, r2aihome)
+if "VIRTUAL_ENV" in os.environ or "R2CORE" in os.environ:
   runpy.run_path(os.path.join(r2aihome, 'r2ai', 'main.py'))
 else:
   os.system(f"cd {r2aihome}; ./run-venv.sh {" ".join(sys.argv[1:])}");
-
-#print(os.path.relpath('./r2ai/main.py', __file__))
-#runpy.run_path(os.path.relpath('./r2ai/main.py', __file__))
