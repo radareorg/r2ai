67212d71319bbd3a50e16b513eac8b96964075e0
pancake <pancake@nopcode.org>
2024-09-17 11:01:02 +0200

    Fix pylint complains from main.py

--- main.py
+++ main.py
@@ -12,6 +12,32 @@ def is_valid_file(parser, arg):
     if not os.path.isfile(arg):
         parser.error(f"The file {arg} does not exist!")
 
+def massage_args(args):
+    runrepl = True
+    if args.command is None:
+        args.command = []
+    if args.webserver:
+        args.command.append("-w")
+    if args.eval:
+        if args.eval == "default":
+            args.command.append("-e")
+            runrepl = False
+        else:
+            args.command.append(f"-e {args.eval}")
+    if args.port:
+        if args.port == "default":
+            runrepl = False
+            args.command.append("-e http.port")
+        else:
+            args.command.append(f"-e http.port={args.port}")
+    if args.model:
+        if args.model == "default":
+            args.command.append("-mm")
+            runrepl = False
+        else:
+            args.command.append(f"-m {args.model}")
+    return runrepl, args
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("bin", nargs="?", type=str)
@@ -25,36 +51,7 @@ def main():
         help="Select model name")
     parser.add_argument("-c", "--command", action="append",
         help="Command to be executed. Can be passed multiple times.")
-    args =  parser.parse_args()
-    runrepl = True
-    if args.webserver:
-        if args.command is None:
-            args.command = []
-        args.command.append("-w")
-    if args.eval:
-        if args.command is None:
-            args.command = []
-        if args.eval == "default":
-            args.command.append("-e")
-            runrepl = False
-        else:
-            args.command.append(f"-e {args.eval}")
-    if args.port:
-        if args.command is None:
-            args.command = []
-        if args.port == "default":
-            print("8080")
-            return
-        args.command.append(f"-e http.port={args.port}")
-    if args.model:
-        if args.command is None:
-            args.command = []
-        if args.model == "default":
-            args.command.append("-mm")
-            runrepl = False
-        else:
-            args.command.append(f"-m {args.model}")
-
+    runrepl, args = massage_args(parser.parse_args())
     r2ai_main(args, args.command, runrepl)
 
 if __name__ == "__main__":
