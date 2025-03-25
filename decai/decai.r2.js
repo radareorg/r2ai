2e35caf147dd73036c46b7cb6cdc38c35a8df672
pancake <pancake@nowsecure.com>
2025-03-25 20:19:42 +0100

    Update decai/decai.r2.js

--- decai/decai.r2.js
+++ decai/decai.r2.js
@@ -794,7 +794,6 @@ Response:
                             break;
                     }
             } catch (e) {
-                console.log('syntax error here?');
                     console.error(out);
                     console.error(e);
                     break;
