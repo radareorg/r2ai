4e6b8ce56681827dfde55f29a9f314a1d6192321
pancake <pancake@nopcode.org>
2025-10-20 21:48:01 +0200

    asidfi

--- src/r2ai.h
+++ src/r2ai.h
@@ -29,11 +29,7 @@
 #define R2_PRINTLN(x) r_cons_println (core->cons, x)
 #define R2_INTERRUPTED() r_cons_is_breaked (core->cons)
 #else
-#define R2_PRINTF(...) r_cons_printf (__VA_ARGS__)
-#define R2_FLUSH() r_cons_flush ()
-#define R2_NEWLINE() r_cons_newline()
-#define R2_PRINTLN(x) r_cons_println(x)
-#define R2_INTERRUPTED() r_cons_is_breaked ()
+#error your radare2 is too old
 #endif
 
 // Tool definition structure
